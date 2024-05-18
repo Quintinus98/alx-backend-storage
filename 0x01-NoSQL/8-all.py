e571cb4ad3a85afef6542e6f2e716784871da69d
David <obododavidsop@gmail.com>
2024-05-18 15:33:04 +0100

    all updated

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -4,4 +4,7 @@
 
 def list_all(mongo_collection):
     """Lists all documents in a collection"""
-    return mongo_collection.find()
+    schools = mongo_collection.find()
+    if mongo_collection.count_documents({}) == 0:
+        return []
+    return schools
--- 0x01-NoSQL/main.py
+++ 0x01-NoSQL/main.py
@@ -1,28 +1,13 @@
 #!/usr/bin/env python3
 """ 8-main """
 from pymongo import MongoClient
-import datetime
 
 list_all = __import__("8-all").list_all
 
 if __name__ == "__main__":
     client = MongoClient("mongodb://127.0.0.1:27017")
     school_collection = client.my_db.school
-    new_posts = [
-        {
-            "name": "Mike",
-            "text": "Another post!",
-            "tags": ["bulk", "insert"],
-            "date": datetime.datetime(2009, 11, 12, 11, 14),
-        },
-        {
-            "name": "Eliot",
-            "title": "MongoDB is fun",
-            "text": "and pretty easy too!",
-            "date": datetime.datetime(2009, 11, 10, 10, 45),
-        },
-    ]
-    result = school_collection.insert_many(new_posts)
     schools = list_all(school_collection)
+    print(schools)
     for school in schools:
         print("[{}] {}".format(school.get("_id"), school.get("name")))
