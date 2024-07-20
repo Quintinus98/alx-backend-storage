a024522fa63eae0040ba3ac0bcb4243214a37baa
David <obododavidsop@gmail.com>
2024-07-20 06:52:28 +0100

    3-glam

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,2 +1,2 @@
 -- Glam rock
-SELECT band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan FROM metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
+SELECT band_name, IFNULL(split, 2022) - IFNULL(formed, 0) AS lifespan FROM metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
