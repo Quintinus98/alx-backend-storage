3a1b7770fee1e886ee25ed6336e04bdb7ef1b8c9
David <obododavidsop@gmail.com>
2024-07-22 19:58:15 +0100

    redis basic

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,6 +2,7 @@
 """REDIS Basics"""
 import redis
 from uuid import uuid4
+from typing import Union
 
 
 class Cache:
@@ -12,8 +13,8 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: str | bytes | float) -> str:
+    def store(self, data: Union[str, bytes, float]) -> str:
         "Store data"
         key = str(uuid4())
-        self._redis.append(key=key, value=data)
+        self._redis.set(name=key, value=data)
         return key
