3a6155ca6cc04057022bbc1cf9f930f81c5f5093
David <obododavidsop@gmail.com>
2024-07-22 20:16:21 +0100

    1 Reading

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -19,19 +19,21 @@ class Cache:
         self._redis.set(name=key, value=data)
         return key
 
-    def get(self, key: str, fn: Optional[Callable]):
+    def get(
+        self, key: str, fn: Optional[Callable]
+    ) -> Union[str, bytes, int, float]:
         """Convert data to fn format or None if not provided"""
         bValue = self._redis.get(name=key)
         if fn is None:
             return bValue
         return fn(bValue)
 
-    def get_str(self, key: str):
+    def get_str(self, key: str) -> str:
         """Convert data to str"""
         bValue = self._redis.get(name=key)
         return bValue.decode("utf-8")
 
-    def get_int(self, key: str, fn: Callable):
+    def get_int(self, key: str, fn: Callable) -> int:
         """Convert data to int"""
         bValue = self._redis.get(name=key)
         try:
